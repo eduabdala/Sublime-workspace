04b6416a6a292310c8b0f6ae42a8c1c1b0861cd0
Eduardo Abdala <eduardoabdala@icloud.com>
2023-09-11 16:38:21 -0300

    ...

--- Projetos/JogoAdivinhacao/adivinhacao.c
+++ Projetos/JogoAdivinhacao/adivinhacao.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main()
 {
@@ -6,7 +9,8 @@ int main()
 	printf("*Bem vindo ao meu jogo de adivinhação*\n");
 	printf("**************************************\n");
 
-	int numeroSecreto = 43;
+	srand(time(NULL));
+	int numeroSecreto = rand() % 100;
 	int chute;
 
 	for(int i = 1; i <= 3; i++) {
@@ -15,12 +19,15 @@ int main()
 		scanf("%d", &chute);
 
 		if (chute == numeroSecreto) {
-			printf("Parabens, voce acertou o Numero Secreto!, seu chute foi %d. \n", chute);
+			printf("Parabens! voce acertou, o numero secreto era:  %d. \n", numeroSecreto) ;
 			break;
-		} else if(chute != numeroSecreto && i < 3) {
-			printf("Voce errou, tente novamente\n");
-		} else {
-			printf("Suas tentativas acaparam e voce nao acertou.\n");
+		} else if(chute > numeroSecreto && i <= 2) {
+			printf("Voce errou, sou chute foi maior que o numero secreto.\n\n");
+		} else if(chute < numeroSecreto && i <= 2) {
+			printf("Voce errou, sou chute foi menor que o numero secreto.\n \n");
+		} 
+		if(chute != numeroSecreto && i == 3) {
+			printf("Que pena parece que suas tentativas acaparam, o numero secreto era %d. \n", numeroSecreto);
 		}
 
 	}
